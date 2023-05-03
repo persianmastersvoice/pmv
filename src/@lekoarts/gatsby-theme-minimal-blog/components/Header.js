2dd3dc883ca0e59eee67203c742ac9d69e65e588
persianmastersvoice <120068514+persianmastersvoice@users.noreply.github.com>
2023-05-03 11:51:02 -0400

    may 3 changes

--- src/@lekoarts/gatsby-theme-minimal-blog/components/Header.js
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/Header.js
@@ -13,14 +13,11 @@ const Header = () => {
   return (
     <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px' }}>
       <Link to="/">
-        <img src="/logo.png" alt="Logo" style={{ height: '80px', width: 'auto', maxWidth: '100%' }} />
+        <img src="/logo.png" alt="Logo" style={{ height: 'auto', width: '100%', maxWidth: '200px' }} />
       </Link>
+
       <div style={{ display: 'flex', alignItems: 'center' }}>
-      <nav style={{ display: 'none', '@media (min-width: 500px)': { display: 'flex' }, opacity: 0.4 }}>
-
-       
-
-          <ul style={{ listStyle: 'none', display: 'flex', gap: '16px', margin: 0 }}>
+          <nav style={{ display: 'none', '@media (min-width: 500px)': { display: 'flex' }, opacity: 0.4 }}>
             <li>
               <Link to="/blog">Blog</Link>
             </li>
@@ -33,29 +30,31 @@ const Header = () => {
             <li>
               <Link to="/contact">Contact</Link>
             </li>
-          </ul>
-        </nav>
-        <button onClick={toggleMenu} style={{ marginLeft: '16px' }}>
+          </nav>
+
+        <button onClick={toggleMenu} style={{ marginLeft: '0px', position: 'relative', zIndex: '1' }}>
           {isOpen ? <FaTimes size={24} /> : <MdMenu size={24} />}
         </button>
-        {isOpen && (
-          <nav style={{ position: 'absolute', top: '80px', left: 0, right: 0, backgroundColor: '#fff', padding: '16px' }}>
-            <ul style={{ listStyle: 'none', display: 'flex', flexDirection: 'column', gap: '16px', margin: 0 }}>
-              <li>
-                <Link to="/blog">Blog</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/persian-music">Persian Music</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
+
+          {isOpen && (
+            <nav className="menu" style={{ position: 'absolute', top: '100px', left: 0, right: 0 }}>
+            <ul style={{ listStyle: 'none', display: 'flex', flexDirection: 'column', gap: '0px', margin: 10 }}>
+        
+            <li style={{ marginLeft: '0px' }}>
+              <Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>ABOUT</Link>
+            </li>
+            <li style={{ marginLeft: '0px' }}>
+              <Link to="/persian-music" style={{ color: '#fff', textDecoration: 'none' }}>PERSIAN MUSIC</Link>
+            </li>
+            <li style={{ marginLeft: '0px' }}>
+              <Link to="/search" style={{ color: '#fff', textDecoration: 'none' }}>SEARCH</Link>
+            </li>
+            <li style={{ marginLeft: '0px' }}>
+              <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>CONTACT</Link>
+            </li>
             </ul>
-          </nav>
-        )}
+            </nav>
+          )}
       </div>
     </header>
   );
--- src/pages/myfirstpost.js
+++ src/pages/myfirstpost.js
@@ -7,30 +7,38 @@ import Layout from "../@lekoarts/gatsby-theme-minimal-blog/components/Layout";
 const FirstPage = () => {
   return (
 <Layout pageTitle="TEST">
-      <img src="../HomePage/FrontPageBaner1.png" alt="first" />
-      <div className="social-icons">
-        <a href="https://www.facebook.com/yourusername" target="_blank" rel="noopener noreferrer">
-          <span className="bi bi-facebook"><FontAwesomeIcon icon={faFacebook} /></span>
-        </a>
-        <a href="https://www.twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
-          <span className="bi bi-twitter"><FontAwesomeIcon icon={faTwitter} /></span>
-        </a>
-        <a href="https://www.instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
-          <span className="bi bi-instagram"><FontAwesomeIcon icon={faInstagram} /></span>
-        </a>
+  
+<div className="full-page-image">
+        <img src="../HomePage/FrontPageBaner1.png" alt="first" className="full-width-image" />
       </div>
-      <footer className="footer" role="contentinfo">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-6">
-              <p className="mb-1" style={{ fontFamily: "Drugs, sans-serif" }}>
-                &copy; Copyright Persian Master's Voice
-                2022
-              </p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      
+      
+       
+  <div class="footer-container">
+  <p className="mb-1" style={{ fontFamily: "Drugs, sans-serif", fontSize: "2vw" }}>
+    &copy; Copyright Persian Master's Voice
+    2022
+</p>
+
+
+
+    <div className="social-icons">
+      <a href="https://www.facebook.com/yourusername" target="_blank" rel="noopener noreferrer">
+        <span className="bi bi-facebook"><FontAwesomeIcon icon={faFacebook} /></span>
+      </a>
+      <a href="https://www.twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
+        <span className="bi bi-twitter"><FontAwesomeIcon icon={faTwitter} /></span>
+      </a>
+      <a href="https://www.instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
+        <span className="bi bi-instagram"><FontAwesomeIcon icon={faInstagram} /></span>
+      </a>
+    </div>
+  </div>
+
+
+
+
+      
     </Layout>
 
   );
