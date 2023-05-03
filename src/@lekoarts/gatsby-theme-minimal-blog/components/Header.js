b0386b423dc4c097fa1b31e742181dc136649bb5
Aaditya Vaze <aadityavikasvaze@gmail.com>
2023-05-03 00:48:43 -0400

    theme customizer added

--- src/@lekoarts/gatsby-theme-minimal-blog/components/Header.js
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/Header.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { Link } from 'gatsby';
-import { MdMenu } from 'react-icons/md';
-import { FaTimes } from 'react-icons/fa';
+import React, { useState } from "react";
+import { Link } from "gatsby";
+import { MdMenu } from "react-icons/md";
+import { FaTimes } from "react-icons/fa";
 
-const Header = () => {
+const HeaderMehdi = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -11,16 +11,37 @@ const Header = () => {
   };
 
   return (
-    <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px' }}>
+    <header
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "16px",
+      }}
+    >
       <Link to="/">
-        <img src="/logo.png" alt="Logo" style={{ height: '80px', width: 'auto', maxWidth: '100%' }} />
+        <img
+          src="/logo.png"
+          alt="Logo"
+          style={{ height: "80px", width: "auto", maxWidth: "100%" }}
+        />
       </Link>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-      <nav style={{ display: 'none', '@media (min-width: 500px)': { display: 'flex' }, opacity: 0.4 }}>
-
-       
-
-          <ul style={{ listStyle: 'none', display: 'flex', gap: '16px', margin: 0 }}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <nav
+          style={{
+            display: "none",
+            "@media (min-width: 500px)": { display: "flex" },
+            opacity: 0.4,
+          }}
+        >
+          <ul
+            style={{
+              listStyle: "none",
+              display: "flex",
+              gap: "16px",
+              margin: 0,
+            }}
+          >
             <li>
               <Link to="/blog">Blog</Link>
             </li>
@@ -35,12 +56,29 @@ const Header = () => {
             </li>
           </ul>
         </nav>
-        <button onClick={toggleMenu} style={{ marginLeft: '16px' }}>
+        <button onClick={toggleMenu} style={{ marginLeft: "16px" }}>
           {isOpen ? <FaTimes size={24} /> : <MdMenu size={24} />}
         </button>
         {isOpen && (
-          <nav style={{ position: 'absolute', top: '80px', left: 0, right: 0, backgroundColor: '#fff', padding: '16px' }}>
-            <ul style={{ listStyle: 'none', display: 'flex', flexDirection: 'column', gap: '16px', margin: 0 }}>
+          <nav
+            style={{
+              position: "absolute",
+              top: "80px",
+              left: 0,
+              right: 0,
+              backgroundColor: "#fff",
+              padding: "16px",
+            }}
+          >
+            <ul
+              style={{
+                listStyle: "none",
+                display: "flex",
+                flexDirection: "column",
+                gap: "16px",
+                margin: 0,
+              }}
+            >
               <li>
                 <Link to="/blog">Blog</Link>
               </li>
@@ -61,4 +99,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default HeaderMehdi;
